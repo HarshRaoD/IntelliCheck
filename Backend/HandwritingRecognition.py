f1adbcf83e6d42aa02902008a0b7e5ddc851c700
Harsh Rao Dhanyamraju <harsh.rao@dhanyamraju.in>
2023-03-11 21:24:08 +0800

    Line splitting

---
+++ Backend/HandwritingRecognition.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def seperate_each_line(img) -> list:
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Getting the threshold
+    th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)
+    
+    # minAreaRect on the nozeros
+    pts = cv2.findNonZero(threshed)
+    ret = cv2.minAreaRect(pts)
+
+    (cx,cy), (w,h), ang = ret
+    if w>h:
+        w,h = h,w
+        ang += 90
+    
+    # Find and draw the upper and lower boundary of each lines
+    hist = cv2.reduce(threshed,1, cv2.REDUCE_AVG).reshape(-1)
+
+    th = 2
+    H,W = img.shape[:2]
+    uppers_y = [y for y in range(H-1) if hist[y]<=th and hist[y+1]>th]
+    lowers_y = [y for y in range(H-1) if hist[y]>th and hist[y+1]<=th]
+
+    threshed = cv2.cvtColor(threshed, cv2.COLOR_GRAY2BGR)
+    
+    # Crop the image
+    cropped_images = []
+    i = 0
+    while(i < len(uppers_y)):
+        cropped_img = threshed[uppers_y[i]:lowers_y[i], 0:W]
+        cropped_images.append(abs(255-cropped_img))
+        i += 1
+
+    return cropped_images
+
+def __test_seperate_each_line(lineNo=0):
+    img = cv2.imread("C:\Harsh Rao Dhanyamraju\Projects\AIfinity Hackathon\AutoRegressive-Alliance\Test\MultiLine_OCR_Test2.jpeg")
+    line_images = seperate_each_line(img)
+    plt.imshow(line_images[lineNo])
+    plt.show()
+
+# __test_seperate_each_line(3)
